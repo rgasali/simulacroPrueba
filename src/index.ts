de7069189918d741ccd308bc6ea64f0e8f12f823
rgasali <96292535+rgasali@users.noreply.github.com>
2022-05-06 13:06:15 -0300

    ejercicio1 resuelto

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,7 @@ let meses: string[] = new Array(
 let mesesDesaprobados: string[] = new Array();
 let peorMes: number;
 let mejorMes: number;
-let comisionA: number[] = new Array(8, 5, 7, 10, 9, 2, 4, 8, 6, 8);
-let comisionB: number[] = new Array(3, 6, 8, 5, 4, 7, 7, 4, 6, 8);
-let comisionC: number[] = new Array(7, 6, 9, 8, 9, 8, 5, 7, 10, 9);
+let promedio: number;
 
 function calcularDesaprobados(notasDelAlumno: Array<number>) {
   notasDelAlumno.forEach((element) => {
@@ -59,21 +57,12 @@ function promediarNota(notasDelAlumno: Array<number>) {
   for (let i = 0; i < notasDelAlumno.length; i++) {
     promedio = promedio + notasDelAlumno[i];
   }
-  return promedio / notasDelAlumno.length;
-}
-
-function mejorPromedio(
-  comision1: Array<number>,
-  comision2: Array<number>,
-  comision3: Array<number>
-) {if(promediarNota(comision1)> promediarNota(comision2)&&
-  promediarNota(comision1)> promediarNota(comision3){
-  return console.log(`el promedio mas alto es ${}`)
-}
-  
+  return Number(promedio / notasDelAlumno.length);
 }
 
 calcularDesaprobados(notasAlumno);
 calcularMenorNota(notasAlumno);
 calcularMejorNota(notasAlumno);
-console.log(`el promedio del alumno es ${promediarNota(notasAlumno)}`);
+console.log(
+  `el promedio del alumno es ${(promedio = promediarNota(notasAlumno))}`
+);
